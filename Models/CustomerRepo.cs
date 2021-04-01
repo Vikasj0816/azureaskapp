a1f9e58fe18ecb8b336ed94bbada5cb8020cc773
Vikasj0816 <vikasj0816@gmail.com>
2021-04-01 11:58:25 +0530

    From VS

---
+++ Models/CustomerRepo.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CustomerManagementProject.Models
+{
+    public class CustomerRepo:IRepo<Customer>
+    {
+        public List<Customer> customers { get; set; }
+        public CustomerRepo()
+        {
+            customers = new List<Customer>();
+        }
+        public void AddItem(Customer item)
+        {
+            customers.Add(item);
+        }
+        public IEnumerable<Customer> GetItems()
+        {
+            return customers;
+        }
+    }
+}
